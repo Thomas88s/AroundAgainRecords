6f44f361e7da7f299c53fc7d2480867349897483
Thomas Witherow <twitherow@bellsouth.net>
2021-03-17 11:59:03 -0500

    added record card

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import RecordApp from './AroundAgainRecords';
-// import { BrowserRouter as Router } from "react-router-dom"
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Store } from "./components/store/Store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecordApp />
+    <Router>
+            <Store />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
