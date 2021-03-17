78d76799d140cf6b5c1143d9a00e3cc2a6a63b8a
Thomas Witherow <twitherow@bellsouth.net>
2021-03-17 15:31:18 -0500

    finally showed on dom

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { AppViews } from "../AppViews"
+import { RecordCard } from "../records/RecordCard"
+// import { AppViews } from "../../AppViews"
+// import { NavBar } from "../nav/NavBar";
 import "./Store.css"
 
 export const Store = () => (
     <>
-        
-       <AppViews />
+        <h1>Around Again Records</h1>
+       
+       <h2>Animals</h2>
+        <article className="records">
+            <RecordCard />
+            <RecordCard />
+            <RecordCard />
+        </article>
     </>
 )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Store } from "./components/store/Store"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    
             <Store />
-    </Router>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
