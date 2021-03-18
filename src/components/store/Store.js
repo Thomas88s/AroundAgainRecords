d7e4dbdd3498689143b71e2f6f7be51acf0cc794
Thomas Witherow <twitherow@bellsouth.net>
2021-03-18 10:11:34 -0500

    really hope this works

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,21 +1,20 @@
 import React from "react"
-// import { RecordCard } from "../records/RecordCard"
-import { AppViews } from "../../AppViews"
-import { NavBar } from "../navigation/NavBar";
+import { RecordCard } from "../records/RecordCard"
 
+// import { AppViews } from "../../AppViews"
+// import { NavBar } from "../nav/NavBar";
 import "./Store.css"
-// Displays the RecordCards and Text in the ListRecords menu
+
 export const Store = () => (
     <>
-        <NavBar />
-        <AppViews />
         <h1>Around Again Records</h1>
        
-       {/* <h2>Records</h2>
+       <h2>Records</h2>
+       
         <article className="records">
             <RecordCard />
             <RecordCard />
             <RecordCard />
-        </article> */}
+        </article>
     </>
 )
